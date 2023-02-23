79c62044e6f27d7c7f4f55ac08e8f2e79ce55661
Menesay <90096742+Menesay@users.noreply.github.com>
2023-02-23 15:11:06 +0000

    Add files via upload

---
+++ upserver.py
@@ -0,0 +1,54 @@
+import http.server
+import socketserver
+import io
+import cgi
+
+PORT = 1401
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+    def doGet(self):
+        self.path = "index.html"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):        
+        r, info = self.deal_post_data()
+        print(r, info, "by: ", self.client_address)
+        f = io.BytesIO()
+        if r:
+            f.write(b"Success\n")
+        else:
+            f.write(b"Failed\n")
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        if f:
+            self.copyfile(f, self.wfile)
+            f.close()      
+
+    def deal_post_data(self):
+        ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
+        pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+        pdict['CONTENT-LENGTH'] = int(self.headers['Content-Length'])
+        if ctype == 'multipart/form-data':
+            form = cgi.FieldStorage( fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'], })
+            print (type(form))
+            
+            try:
+                if isinstance(form["file"], list):
+                    for record in form["file"]:
+                        open("./%s"%record.filename, "wb").write(record.file.read())
+                else:
+                    open("./%s"%form["file"].filename, "wb").write(form["file"].file.read())
+            except IOError:
+                    return (False, "Can't create file to write")
+            
+        return (True, "Files uploaded")
+
+Handler = CustomHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
